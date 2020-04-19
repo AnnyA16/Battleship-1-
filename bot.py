cdc322487388a390914838a272b172e812055850
AnnyA16 <annyantigua@yahoo.com>
2020-04-18 21:04:03 -0400

    this code places ship at a random location on the board

--- bot.py
+++ bot.py
@@ -15,8 +15,18 @@ async def on_ready():
 
 @bot.command(name="hi", help="Says hello")
 async def say_hello(ctx):
-    await ctx.send(f"Hi {ctx.author.display_name}")
+    await ctx.send(f"Hi {ctx.author.display_name})
 
+@bot.command(name="random_row", help="Selects a random row")
+async def random_row(grid):
+    await ctx.send(random.randint(0,6))
+
+@bot.command(name="random_col", help="Selects a random column")
+async def random_col(board):
+    await ctx.send(random.randint(0,6))
+
+ship_row = random_row(grid)
+print(ship_row)
+ship_col = random_col(grid)
+print(ship_col)
 bot.run(TOKEN)
-
-
