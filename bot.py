9b9c63307687d2aaf510c64b54e38584311a767a
nat-pat <60788652+nat-pat@users.noreply.github.com>
2020-04-19 22:17:15 -0400

    This code allows you to guess where your opponent's ship is by column and row. The only input that does not work is the number 6.

--- bot.py
+++ bot.py
@@ -17,6 +17,23 @@ async def on_ready():
 async def say_hello(ctx):
     await ctx.send(f"Hi {ctx.author.display_name}")
 
+for turn in range(6):
+    guess_row = input("Guess Row:")
+    guess_col = input("Guess Col:")
+    if guess_row == battle_row and guess_col == battle_col:
+      print ("Ahhhh you got me, darn it😞! Good game.")
+      break
+    if turn == 5:
+            print ("aHaH, you lost😂😂😂!! But good game.")
+            print ("My ship was here: [" + str(battle_row) + "][" + str(battle_col) + "]")
+    else:
+            if (int(guess_row) < 1 or int(guess_row) >= 7) or (int(guess_col) < 1 or int(guess_col) >= 7):
+                print ("Dummy, that's not even in the ocean😋")
+            elif(grid[int(guess_row)][int(guess_col)] == "X"):
+                print ("Sillyyyyy🙈, try again.")
+            print (turn + 1)
+            print_grid(grid)
+
+
+
 bot.run(TOKEN)
-
-
